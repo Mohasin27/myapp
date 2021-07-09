b2a5c027ab8ee426f9bbe986611941fac5e39152
Mohasin27 <mohasinmomin@gmail.com>
2021-07-09 12:47:05 +0530

    Added home content

---
+++ src/Components/HomeCategoryBanner.js
@@ -0,0 +1,46 @@
+import React from 'react';
+
+
+
+function HomeCategoryBanner() {
+    return (
+        <div className="cat-banner-wrap">
+            <div className="cat-banner-container">
+                <div className="row cat-space">                         
+                <div className="cat-banne-layout-top-left cat_space_item col-lg-4 col-md-4 col-12">
+                    <div className="banner_hzoom">
+                        <div className="banner_img"></div>
+                        <a href="#url"></a>
+                        <div className="banner_html">
+                            <h3>lighting collections</h3>
+                            <h4>Explorer</h4>
+                        </div>
+                    </div>
+                </div>
+                <div className="cat-banner-layout-bottom-right cat_space_item col-lg-4 col-md-4 col-12">
+                    <div className="banner_hzoom">
+                        <div className="banner_img"></div>
+                        <a href="#url"></a>
+                        <div className="banner_html">
+                            <h3>audio decor</h3>
+                            <h4>view collections</h4>
+                        </div>
+                    </div>
+                </div>
+                <div className="cat-banner-layout-top-left-black cat_space_item col-lg-4 col-md-4 col-12">
+                    <div className="banner_hzoom" >
+                        <div className="banner_img"></div>
+                        <a href="#url"></a>
+                        <div className="banner_html">
+                            <h3>new arrivals</h3>
+                            <h4>shop now</h4>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            </div>
+        </div>
+    )
+}
+
+export default HomeCategoryBanner;
